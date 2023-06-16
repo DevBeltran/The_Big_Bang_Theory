f9620bad28dace952af1ad6e76090735c9dd6457
Pedro <developerpgb@gmail.com>
2023-06-16 18:03:37 +0100

    Initial Commit

---
+++ The Big Bang/The_Big_BangApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Big_BangApp.swift
+//  The Big Bang
+//
+//  Created by Pedro  on 16/6/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Big_BangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
